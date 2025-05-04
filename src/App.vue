3cc84d55b97bc9bb7274762e76d0b9e8e89da429
jb <dev.jbcu10@gmail.com>
2025-05-04 19:24:46 +0200

    test vercel

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="flex justify-center items-center min-h-screen bg-blue-500 text-white">
-    <h1 class="text-4xl font-bold">Site under development</h1>
+    <h1 class="text-4xl font-bold">Hello I'm John Ben</h1>
   </div>
 </template>
 
